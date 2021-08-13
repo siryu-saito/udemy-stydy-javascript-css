60a8906953bd8bd23900bb0649a0cbaa248019da
siryu-saito <saito.s.ryu@gmail.com>
2021-08-13 21:25:10 +0900

    DOMContentLoadedとLoadの違い

--- 04_JavaScriptの基礎を固める/100_DOMイベント（ページの初期化）/start/main.js
+++ 04_JavaScriptの基礎を固める/100_DOMイベント（ページの初期化）/start/main.js
@@ -1,10 +1,14 @@
-const dcl = document.querySelector('.dcl');
-const load = document.querySelector('.load');
+const h1 = document.querySelector('h1');
 
-document.addEventListener("DOMContentLoaded", function () {
-    dcl.classList.add('done');
-});
+h1.style.color = 'red'
 
-window.addEventListener("load", function () {
-    load.classList.add('done');
-});
+// const dcl = document.querySelector('.dcl');
+// const load = document.querySelector('.load');
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     dcl.classList.add('done');
+// });
+
+// window.addEventListener("load", function () {
+//     load.classList.add('done');
+// });
